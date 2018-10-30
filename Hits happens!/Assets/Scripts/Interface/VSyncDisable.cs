9b14345b810f6cf63dcdaaef6c5d3823101ef674
Paweł Szynal <pawel.szynal@email.com>
2018-10-30 23:44:14 +0100

    Base Scene

---
+++ Hits happens!/Assets/Scripts/Interface/VSyncDisable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Misc
+{
+    public class VSyncDisable : MonoBehaviour
+    {
+        private void Start()
+        {
+            QualitySettings.vSyncCount = 0;
+        }
+    }
+}
