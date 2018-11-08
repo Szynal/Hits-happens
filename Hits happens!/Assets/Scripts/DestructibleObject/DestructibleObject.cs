86d1fb632f52e4e9070a00cc115aa01cbe811a1b
Paweł Szynal <pawel.szynal@email.com>
2018-11-08 23:16:22 +0100

    DestructibleObject
    Awake

---
+++ Hits happens!/Assets/Scripts/DestructibleObject/DestructibleObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.DestructibleObject
+{
+    public class DestructibleObject : MonoBehaviour
+    {
+        public string ObjectName = string.Empty;
+        public GameObject MeinObject = null;
+
+        [HideInInspector]
+        public List<GameObject> ConnectedObjects = null;
+        public List<Collider> BallisticsColliders = new List<Collider>();
+
+        protected virtual void Awake()
+        {
+            AddConnectedObjectsToList();
+            FindBallisticsColliders();
+        }
+
+        private void AddConnectedObjectsToList()
+        {
+            if (MeinObject == null)
+            {
+                return;
+            }
+
+            ConnectedObjects = new List<GameObject>();
+
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject != MeinObject)
+                {
+                    ConnectedObjects.Add(child.gameObject);
+                }
+            }
+        }
+
+        private void FindBallisticsColliders()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
