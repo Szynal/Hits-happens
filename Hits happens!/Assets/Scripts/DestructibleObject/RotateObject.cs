6755125f3c177948a5c5eba395f49291801235a3
Krzysztof Sobocinski <kszys96@outlook.com>
2018-11-10 19:43:01 +0100

    Added Joystic + objectrotation

---
+++ Hits happens!/Assets/Scripts/DestructibleObject/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+
+    public Joystick joystic;
+
+    [SerializeField] public float rotationMultiplier = 1f;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	    if (joystic.Horizontal != 0f || joystic.Vertical != 0)
+	    {
+	        gameObject.transform.rotation *= Quaternion.Euler(joystic.Horizontal*rotationMultiplier, joystic.Vertical* rotationMultiplier, 0f);
+	    }
+		
+	}
+}
